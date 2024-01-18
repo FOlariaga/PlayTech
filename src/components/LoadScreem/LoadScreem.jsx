275e27e0e5ae419b73c0b27de872d6ccbd3f06d2
FOlariaga <olariagafederico2@gmail.com>
2024-01-18 06:41:05 -0300

    correccion de rutas de imagenes

--- src/components/LoadScreem/LoadScreem.jsx
+++ src/components/LoadScreem/LoadScreem.jsx
@@ -5,7 +5,7 @@ const LoadScreem = () => {
         <section className={classes.secLoadScreem}>
             <h1 className={classes.titleLoadScreem}>cargando . . .</h1>
             <div className= {classes.loading}>
-                <img className={classes.pacman} src="/public/images/Pac-man_open.png" alt=""  />
+                <img className={classes.pacman} src="https://drive.google.com/file/d/1244vF7cWL4jvSSEynAUr17ulYJOln7K4/view" alt=""  />
                 <img className={classes.pacmanClose} src="/public/images/Pac-man_close.png" alt="" />
                 <div className={classes.pastilla}>.</div>
                 <div className={classes.pastilla}>.</div>
